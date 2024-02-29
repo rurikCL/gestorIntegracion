d530f5887ebc7dd0832ea4f98e670b223ae3340f
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-02-29 13:48:37 -0300

    [UPDATE] Cambios generales

--- app/Http/Controllers/Flujo/FlujoHubspotController.php
+++ app/Http/Controllers/Flujo/FlujoHubspotController.php
@@ -93,7 +93,7 @@ class FlujoHubspotController extends Controller
                             }
 
                         } else {
-                            $nombre = $data->properties['firstname'] . ' ' . $data->properties['lastname'] ?? '';
+                            $nombre = ($data->properties['firstname'] ?? '') . ' ' . ($data->properties['lastname'] ?? '');
                             $email = $data->properties['email'] ?? '';
                             $telefono = $data->properties['phone'] ?? '';
                             $rut = $data->properties['rut'] ?? '';

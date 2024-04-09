255ad74f448db8ee774fd67dd617320aab7f6b95
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-04-09 11:00:01 -0400

    [UPDATE] Generador de OC (Usuario)

--- app/Filament/Resources/MA/MAUsuariosResource/Pages/EditMAUsuarios.php
+++ app/Filament/Resources/MA/MAUsuariosResource/Pages/EditMAUsuarios.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\MA\MAUsuariosResource\Pages;
 
 use App\Filament\Resources\MA\MAUsuariosResource;
+use App\Models\MA\MA_Usuarios;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditMAUsuarios extends EditRecord
 {
@@ -14,6 +16,12 @@ class EditMAUsuarios extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Actions\Action::make('link')
+                ->label("Link Roma")
+                ->url(fn () => "https://apps1.pompeyo.cl/?id=" .$this->record->ID ."&token=6461433ef90325a215111f2af1464b2d09f2ba23", true)
+                ->icon('heroicon-o-link')
+                ->color('success')
+                ->visible(Auth::user()->isAdmin()),
         ];
     }
 }

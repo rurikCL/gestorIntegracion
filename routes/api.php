9be32465fb052afb82e223e3b4e2fc285f3368ac
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-05-20 12:51:12 -0400

    [UPDATE] API Santander

--- routes/api.php
+++ routes/api.php
@@ -60,5 +60,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/financieras/enviar', [FinancierasController::class, 'enviarFinancieras']);
     Route::post('/santander/calculadora', [FinancierasController::class, 'calculadoraSantander']);
+    Route::post('/santander/solicitud', [FinancierasController::class, 'creditoSantander']);
 
 });

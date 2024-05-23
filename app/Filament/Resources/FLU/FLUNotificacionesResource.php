dd4c3ac9c35154996fa53f6e2935b0395a552851
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-05-23 17:58:47 -0400

    [UPDATE] Notificaciones

--- app/Filament/Resources/FLU/FLUNotificacionesResource.php
+++ app/Filament/Resources/FLU/FLUNotificacionesResource.php
@@ -83,7 +83,8 @@ class FLUNotificacionesResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array

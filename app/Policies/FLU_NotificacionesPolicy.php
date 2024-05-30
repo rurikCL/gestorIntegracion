371b442a878b4f76254691154ccb15b17f325d59
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-05-30 17:42:27 -0400

    [UPDATE] Policies

---
+++ app/Policies/FLU_NotificacionesPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class FLU_NotificacionesPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function viewAny(User $user)
+    {
+        //
+        return $user->isAdmin();
+    }
+
+    public function create(User $user)
+    {
+        //
+        return $user->isAdmin();
+
+    }
+    public function update(User $user)
+    {
+        //
+        return $user->isAdmin();
+
+    }
+}
---
+++ app/Policies/OC_purchase_ordersPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class OC_purchase_ordersPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function viewAny(User $user)
+    {
+        //
+        return $user->isAdmin();
+    }
+
+    public function create(User $user)
+    {
+        //
+        return $user->isAdmin();
+
+    }
+    public function update(User $user)
+    {
+        //
+        return $user->isAdmin();
+
+    }
+}

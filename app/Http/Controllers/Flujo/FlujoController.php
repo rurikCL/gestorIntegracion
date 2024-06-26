38fc92f1d1bc105030eed7f928f2c761337fe898
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-06-25 21:33:25 -0400

    [UPDATE] fecha autored transactions

--- app/Http/Controllers/Flujo/FlujoController.php
+++ app/Http/Controllers/Flujo/FlujoController.php
@@ -1606,9 +1606,9 @@ class FlujoController extends Controller
 
             $req['data'] = [
 //                "from" => Carbon::now()->subDays(1)->format('Y-m-d'),
-                "from" => "2024-04-01",
+                "from" => "2024-05-01",
 //                "to" => Carbon::now()->format('Y-m-d'),
-                "to" => "2024-04-30",
+                "to" => "2024-05-31",
             ];
 
 

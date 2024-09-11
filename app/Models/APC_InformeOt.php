44ebc53d5956b837c8fd4dd537d163f141179ab6
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-09-11 16:16:00 -0300

    [UPDATE] Robots

---
+++ app/Models/APC_InformeOt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class APC_InformeOt extends Model
+{
+    use HasFactory;
+}

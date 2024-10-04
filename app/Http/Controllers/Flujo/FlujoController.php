549aaa39c51ff2e32ece053de8c7bf533768b91a
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-10-04 11:06:32 -0300

    [UPDATE] UF

--- app/Http/Controllers/Flujo/FlujoController.php
+++ app/Http/Controllers/Flujo/FlujoController.php
@@ -1839,7 +1839,7 @@ class FlujoController extends Controller
         if ($arrayData) {
             foreach ($arrayData->UFs as $data) {
                 $fecha = $data->Fecha;
-                $valor = floatval($data->valor);
+                $valor = floatval($data->Valor);
                 $indicador = MA_IndicadorMonetario::updateOrCreate(
                     [
                         'FechaIndicador' => $fecha,

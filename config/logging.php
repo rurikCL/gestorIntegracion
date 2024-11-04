2c6bf1606fa7701489fb16bb0b6d283b2f04e80d
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-11-04 13:21:01 -0300

    [UPDATE] flujo hubspot sync

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'robots' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/robots.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ]
     ],
 
 ];

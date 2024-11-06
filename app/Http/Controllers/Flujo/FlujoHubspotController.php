48bd32077581690ceb7b75af88f6064daedd2975
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-11-06 15:37:23 -0300

    [UPDATE] flujo hubspot sync

--- app/Http/Controllers/Flujo/FlujoHubspotController.php
+++ app/Http/Controllers/Flujo/FlujoHubspotController.php
@@ -797,7 +797,7 @@ class FlujoHubspotController extends Controller
                     echo $e->getMessage();
                     $contacto = $client->crm()->contacts()->searchApi()->doSearch($searchRequest)->getResults();
                     if($contacto){
-                        dd($contacto);
+                        print_r($contacto, true);
                         $idContacto = $contacto->getId();
                     }
                 }

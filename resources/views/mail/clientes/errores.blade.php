948dc8e5c7954ca48e03848caf149a09962863fd
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-11-07 13:45:08 -0300

    [UPDATE] sync hubspot

--- resources/views/mail/clientes/errores.blade.php
+++ resources/views/mail/clientes/errores.blade.php
@@ -1,9 +1,9 @@
 <body>
 <h1>Se han detectado error en los datos de los clientes</h1>
 @foreach($correos as $correo)
-    <div>Cliente : {{$correo->idCliente}}</div>
-    <div>Correo : {{$correo->email}}</div>
-    <div><a href="https://apps3.pompeyo.cl/admin/m-a/m-a-clientes/{{$correo->idCliente}}/edit"> Revisar </a></div>
+    <div>Cliente : {{$correo["idCliente"]}}</div>
+    <div>Correo : {{$correo["email"]}}</div>
+    <div><a href="https://apps3.pompeyo.cl/admin/m-a/m-a-clientes/{{$correo["idCliente"]}}/edit"> Revisar </a></div>
     <hr>
 @endforeach
 </body>

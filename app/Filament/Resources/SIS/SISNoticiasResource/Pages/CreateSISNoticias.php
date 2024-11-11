a7216c06e2e0d6076a5298d4d0254fe0c72f063f
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-11-11 10:47:00 -0300

    [UPDATE] editor noticias

--- app/Filament/Resources/SIS/SISNoticiasResource/Pages/CreateSISNoticias.php
+++ app/Filament/Resources/SIS/SISNoticiasResource/Pages/CreateSISNoticias.php
@@ -3,10 +3,21 @@
 namespace App\Filament\Resources\SIS\SISNoticiasResource\Pages;
 
 use App\Filament\Resources\SIS\SISNoticiasResource;
-use Filament\Actions;
+use Carbon\Carbon;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateSISNoticias extends CreateRecord
 {
     protected static string $resource = SISNoticiasResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+
+        $data['FechaCreacion'] = Carbon::now()->format('Y-m-d H:i:s');
+        $data['EventoCreacionID'] = 1;
+        $data['UsuarioCreacionID'] = Auth::user()->id;
+
+        return $data;
+    }
 }

4d1f98da807cb0e39a41e43ef8485da60a0c7be5
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-12-09 10:54:46 -0300

    [UPDATE] Mantenedor Accesorios

--- app/Policies/VT_ElementosFinanciadosSubTiposPolicy.php
+++ app/Policies/VT_ElementosFinanciadosSubTiposPolicy.php
@@ -17,19 +17,19 @@ class VT_ElementosFinanciadosSubTiposPolicy
     public function viewAny(User $user)
     {
         //
-        return $user->isRole(['admin']);
+        return $user->isRole(['admin','analista']);
     }
 
     public function create(User $user)
     {
         //
-        return $user->isRole(['admin']);
+        return $user->isRole(['admin','analista']);
 
     }
     public function update(User $user)
     {
         //
-        return $user->isRole(['admin']);
+        return $user->isRole(['admin','analista']);
 
     }
 }

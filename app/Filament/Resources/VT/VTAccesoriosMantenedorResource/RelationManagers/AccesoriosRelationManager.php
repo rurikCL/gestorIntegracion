ff5d2493bc2eea8088ae673d8812dd3870e1d525
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-12-09 16:16:25 -0300

    [UPDATE] Mantenedor Accesorios

--- app/Filament/Resources/VT/VTAccesoriosMantenedorResource/RelationManagers/AccesoriosRelationManager.php
+++ app/Filament/Resources/VT/VTAccesoriosMantenedorResource/RelationManagers/AccesoriosRelationManager.php
@@ -94,11 +94,11 @@ class AccesoriosRelationManager extends RelationManager
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+//                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+//                    Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }

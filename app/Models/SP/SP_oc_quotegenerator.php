cc650389ae184ff9fe139f56a11ead6d83ac06ec
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2024-12-30 13:46:44 -0300

    [UPDATE] MA SUCURSALES

--- app/Models/SP/SP_oc_quotegenerator.php
+++ app/Models/SP/SP_oc_quotegenerator.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\SP;
 
+use App\Models\MA\MA_Usuarios;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,4 +17,9 @@ class SP_oc_quotegenerator extends Model
         'branchOffice_id',
         'user_id',
     ];
+
+    public function usuarios()
+    {
+        return $this->hasOne(MA_Usuarios::class, 'ID', 'user_id');
+    }
 }

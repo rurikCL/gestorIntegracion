b1d3521ec55632c8ec6767757b00e06d2d07a830
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-03-31 13:01:34 -0300

    [UPDATE] update leads sincronizacion

--- app/Http/Controllers/MonitorFlujoController.php
+++ app/Http/Controllers/MonitorFlujoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Mail\MonitorError;
 use App\Models\FLU_Monitor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class MonitorFlujoController extends Controller
@@ -18,51 +19,67 @@ class MonitorFlujoController extends Controller
     {
         $this->FlujoID = $FlujoID;
         $this->accion = $accion;
+        $this->registrarInicio();
     }
 
     public function registrarInicio($estado = 'INICIO')
     {
-        $monitor = new FLU_Monitor();
-        $monitor->FlujoID = $this->FlujoID;
-        $monitor->Accion = $this->accion;
-        $monitor->Estado = $estado;
-        $monitor->FechaInicio = date("Y-m-d H:i:s");
-        $monitor->save();
+        if ($this->id) {
 
-        $this->id = $monitor->id;
-        return $monitor->id;
+            $monitor = new FLU_Monitor();
+            $monitor->FlujoID = $this->FlujoID;
+            $monitor->Accion = $this->accion;
+            $monitor->Estado = $estado;
+            $monitor->FechaInicio = date("Y-m-d H:i:s");
+            $monitor->save();
+
+            $this->id = $monitor->id;
+            return $monitor->id;
+        } else {
+            return $this->id;
+        }
     }
 
-    public function setMessage($mensaje){
+    public function setMessage($mensaje)
+    {
         $monitor = FLU_Monitor::where('id', $this->id)->first();
         $monitor->Mensaje = $mensaje;
         $monitor->save();
     }
 
-    public function setAction($accion){
+    public function setAction($accion)
+    {
         $monitor = FLU_Monitor::where('id', $this->id)->first();
         $monitor->Accion = $accion;
         $monitor->save();
     }
 
 
-    public function registrarFin($estado = 'TERMINADO'){
+    public function registrarFin($estado = 'TERMINADO')
+    {
         $monitor = FLU_Monitor::where('id', $this->id)->first();
-        $monitor->Estado = $estado;
-        $monitor->FechaTermino = date("Y-m-d H:i:s");
-        $monitor->Duracion = strtotime($monitor->FechaTermino) - strtotime($monitor->FechaInicio);
-        $monitor->save();
+        if ($monitor) {
+            $monitor->Estado = $estado;
+            $monitor->FechaTermino = date("Y-m-d H:i:s");
+            $monitor->Duracion = strtotime($monitor->FechaTermino) - strtotime($monitor->FechaInicio);
+            $monitor->save();
+        } else {
+            Log::error("Error al registrar FIN de Monitor");
+        }
     }
 
-    public function registrarError($mensaje = "Error"){
+    public function registrarError($mensaje = "Error")
+    {
         $monitor = FLU_Monitor::where('id', $this->id)->first();
-        $monitor->Estado = 'ERROR';
-        $monitor->Mensaje = $mensaje;
-        $monitor->FechaTermino = date("Y-m-d H:i:s");
-        $monitor->Duracion = strtotime($monitor->FechaTermino) - strtotime($monitor->FechaInicio);
-        $monitor->save();
+        if ($monitor) {
+            $monitor->Estado = 'ERROR';
+            $monitor->Mensaje = $mensaje;
+            $monitor->FechaTermino = date("Y-m-d H:i:s");
+            $monitor->Duracion = strtotime($monitor->FechaTermino) - strtotime($monitor->FechaInicio);
+            $monitor->save();
 
-        Mail::to('cristian.fuentealba@pompeyo.cl')->cc(['rodrigo.larrain@pompeyo.cl', 'rurik.neologik@gmail.com'])
-            ->send(new MonitorError($monitor));
+            Mail::to('cristian.fuentealba@pompeyo.cl')->cc(['rodrigo.larrain@pompeyo.cl', 'rurik.neologik@gmail.com'])
+                ->send(new MonitorError($monitor));
+        }
     }
 }

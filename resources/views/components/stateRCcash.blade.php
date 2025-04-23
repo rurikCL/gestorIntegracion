97d6d6a66f08acb20572ffc317634973f631fa97
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-04-23 10:12:54 -0400

    [UPDATE] Cajachica

--- resources/views/components/stateRCcash.blade.php
+++ resources/views/components/stateRCcash.blade.php
@@ -4,4 +4,6 @@
     <span class="inline-flex items-center rounded-md  px-2 py-1 text-xs font-medium ring-1 ring-inset ring-green-600/20"><i class="text-2xs fa fa-circle text-green-500 pr-2"> </i> En Proceso</span>
 @elseif( $getState() === 2 )
     <span class="inline-flex items-center rounded-md  px-2 py-1 text-xs font-medium ring-1 ring-inset ring-green-600/20 "><i class="text-2xs heroicon-o-rectangle-stack text-red-500 pr-2"> </i> Aprobado</span>
+@elseif( $getState() === 3 )
+    <span class="inline-flex items-center rounded-md  px-2 py-1 text-xs font-medium ring-1 ring-inset ring-green-600/20 "><i class="text-2xs heroicon-o-rectangle-stack text-red-800 pr-2"> </i> Rechazado</span>
 @endif

9370261592cbcfb4144cfea099326aced5e0e50d
cristian_fuentealba <cristian_fuentealba@pompeyo.cl>
2025-07-11 17:13:30 -0400

    [UPDATE] Integracion cambios en Lead, desde KIA / Externo

--- app/Filament/Resources/API/ApiSolicitudesResource/RelationManagers/SolicitudesRelationManager.php
+++ app/Filament/Resources/API/ApiSolicitudesResource/RelationManagers/SolicitudesRelationManager.php
@@ -52,6 +52,11 @@ class SolicitudesRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('FechaResolucion')->label('Hora resolucion')
                     ->dateTime("H:i:s"),
                 Tables\Columns\TextColumn::make('CodigoRespuesta'),
+                Tables\Columns\TextColumn::make('contadorHijos')
+                ->state(fn($record) => $record->subsolicitudes->count())
+                    ->label('Subsolicitudes')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
